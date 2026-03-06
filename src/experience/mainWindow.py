4f967bb8209f930f7e868c8e87c4122246bd1484
Eduardo Goncalvez <eduardogoncalvez00@gmail.com>
2026-03-06 17:45:59 -0500

    fix: use QWidget container for main window layout

--- src/experience/mainWindow.py
+++ src/experience/mainWindow.py
@@ -1,5 +1,6 @@
-from PySide6.QtWidgets import QMainWindow
+from PySide6.QtWidgets import QMainWindow, QWidget, QHBoxLayout
 from src.experience.sideBar import Sidebar
+from src.experience.pages.dashboard import dashboard
 
 class MainWindow(QMainWindow):
     def __init__(self):
@@ -9,8 +10,16 @@ class MainWindow(QMainWindow):
         self.setMinimumWidth(800)
         self.setMinimumHeight(600)
 
-        self.sidebar = Sidebar(self)
-        self.setCentralWidget(self.sidebar)
+        # Main container for the main window
+        self.main_container = QWidget()
+        self.main_container.setLayout(QHBoxLayout())
 
+        self.sidebar = Sidebar(self)
+        self.main_container.layout().addWidget(self.sidebar)
+
+        self.dashboard = dashboard(self)
+        self.main_container.layout().addWidget(self.dashboard)
+
+        self.setCentralWidget(self.main_container)
         self.show()
 
