c7e94fae95c7f1a4ac61025620c3e418039125fd
Eduardo Goncalvez <eduardogoncalvez00@gmail.com>
2026-03-05 22:43:31 -0500

    Fix: Add label to sidebar container and place it in main Window

--- src/experience/mainWindow.py
+++ src/experience/mainWindow.py
@@ -1,8 +1,16 @@
 from PySide6.QtWidgets import QMainWindow
+from src.experience.sideBar import Sidebar
 
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
         self.setWindowTitle("Study Tracker Partner")
+
+        self.setMinimumWidth(800)
+        self.setMinimumHeight(600)
+
+        self.sidebar = Sidebar(self)
+        self.setCentralWidget(self.sidebar)
+
         self.show()
 
--- src/experience/sideBar.py
+++ src/experience/sideBar.py
@@ -1,5 +1,9 @@
-from src.experience.mainWindow import MainWindow
-from PySide6.QtWidgets import QWidget, QVBoxLayout, QPushButton
+from PySide6.QtWidgets import QWidget, QVBoxLayout, QPushButton, QLabel
+
+from typing import TYPE_CHECKING # avoid circular imports
+
+if TYPE_CHECKING: 
+    from src.experience.mainWindow import MainWindow
 
 class Sidebar(QWidget):
     def __init__(self, main_window : MainWindow):
@@ -7,6 +11,8 @@ class Sidebar(QWidget):
         self.main_window = main_window
         self.layout = QVBoxLayout() 
         self.setLayout(self.layout)
+
+        self.add_items(QLabel("Sidebar"))
 
     def add_items(self, widget: QWidget):
         self.layout.addWidget(widget)
