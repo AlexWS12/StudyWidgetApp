96b7ea419b6459a1938f069b97cf857a5f4599f0
Eduardo Goncalvez <eduardogoncalvez00@gmail.com>
2026-03-08 14:42:55 -0400

    fix: move petWindow to application container and position it on bottom right of the screen

--- src/experience/pages/virtualPet.py
+++ src/experience/pages/virtualPet.py
@@ -1,15 +1,8 @@
 from PySide6.QtWidgets import QWidget, QVBoxLayout, QLabel
 
-from src.experience.petWindow import petWindow
-
-
 class virtualPet(QWidget):
     def __init__(self, parent: None):
         super().__init__(parent)
         self.layout = QVBoxLayout()
         self.setLayout(self.layout)
         self.layout.addWidget(QLabel("Virtual Pet"))
-
-        #  Temp: Opens the petWindow Window
-        self.pet_window = petWindow()
-        self.pet_window.show()
