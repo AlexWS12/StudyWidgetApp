c88a10e0a6d4f89526a6fb93bbaa3aed61efd67b
avuto001 <vutovalex@gmail.com>
2026-03-21 21:37:58 -0400

    Updated menu
    Included Mac support

--- src/vision/menu.py
+++ src/vision/menu.py
@@ -3,7 +3,15 @@ import cv2 as cv
 from rich.console import Console
 from rich.panel import Panel
 import questionary
-import msvcrt
+import sys
+
+if sys.platform == "win32":
+    import msvcrt
+else:
+    msvcrt = None
+
+
+
 
 from camera import Camera
 from detectors.phone_calibration import PhoneCalibration
@@ -65,8 +73,9 @@ def main() -> None:
 
         # Drain any keys that were pressed while the previous option was running so
         # they don't immediately fire the next questionary prompt.
-        while msvcrt.kbhit():
-            msvcrt.getch()
+        if msvcrt:
+            while msvcrt.kbhit():
+             msvcrt.getch()
 
         choice = questionary.select(
             "Choose an option:",
